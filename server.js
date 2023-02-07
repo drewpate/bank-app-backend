0a610d4a9ecabfe9a88e3d41af74927acb5751e2
Drew Pate <andrewcpate@gmail.com>
2023-02-06 21:25:54 -0500

    removes the rest of swagger from server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const userRouter = require("./routes/api/users-route");
-const swaggerRoutes = require("./routes/api/swagger");
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,9 +18,6 @@ app.use(express.json());
 
 // use routes
 app.use("/api/users", userRouter);
-
-//swagger
-app.use("/api", swaggerRoutes);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
